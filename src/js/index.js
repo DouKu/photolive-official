ec8bcb511097d15264500c13ffeab9d6b0f73c72
lgybetter <437675103@qq.com>
2018-09-09 23:26:51 +0800

    :art: 修改居中

--- src/js/index.js
+++ src/js/index.js
@@ -3,5 +3,9 @@ import '../style/sass/main.scss'
 var mySwiper = new Swiper ('.swiper-container', {
   direction: 'vertical',
   loop: false,
-  mousewheel: true
+  mousewheel: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable :true
+  }
 });

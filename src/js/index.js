ebf033023a3517858341ea3f2b7c5a4b0326b875
lgybetter <437675103@qq.com>
2018-09-16 21:31:08 +0800

    :art: 修改主页不为轮播

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,11 @@
 import '../style/sass/main.scss'
 
-var mySwiper = new Swiper ('.swiper-container', {
-  direction: 'vertical',
-  loop: false,
-  mousewheel: true,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable :true
-  }
-});
+// var mySwiper = new Swiper ('.swiper-container', {
+//   direction: 'vertical',
+//   loop: false,
+//   mousewheel: true,
+//   pagination: {
+//     el: '.swiper-pagination',
+//     clickable :true
+//   }
+// });
